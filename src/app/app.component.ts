b65eefc600d181844d24de82def22507c7822b0b
Mahfuzul Haque <MHaque@HCX-LP-1121>
2018-08-22 17:44:00 +1000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from './service/todo.service';
+import { Todo } from './model/todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  todos: Todo[];
+
+  ngOnInit(): void {
+    this.todoService.loadTodos().subscribe(data => {
+      this.todos = data.json();
+    });
+  }
+
+  constructor(private todoService: TodoService) {
+  }
 }
---
+++ src/app/service/todo.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class TodoService {
+    private readonly url = '/tasks';
+
+    constructor(private http: Http) {
+    }
+
+    loadTodos() {
+        return this.http.get(this.url);
+    }
+}
